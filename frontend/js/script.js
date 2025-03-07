b5360bc66b683b57f2f97f2dfad5bfb1f40f0b91
Joao Victor Moreira da Costa <contato.joaovictormcosta@gmail.com>
2025-03-07 11:50:14 -0300

    Primeiro commit pelo Termux

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -84,7 +84,7 @@ const handleLogin = (event) =>{
     login.style.display = "none";
     chat.style.display = "flex";
 
-    websocket = new WebSocket("ws://localhost:8080");
+    websocket = new WebSocket("wss://github.com/JoaoVictorMCosta/js-chat.git");
      websocket.onmessage = processMessage;
 };
 
